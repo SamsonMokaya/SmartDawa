16e42f52e3d321d7ae0f14c58fbb58e672fbd5da
SamsonMokaya <mokayasamson950@gmail.com>
2023-07-18 17:47:20 +0300

    Imports

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv  = require("dotenv").config();
 const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
 
 const app = express();
 

7f59cac54c00cd4484246397b9a7fc02a0d9fc5d
Samson Mokaya <68181226+SamsonMokaya@users.noreply.github.com>
2023-10-13 09:32:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const express = require("express");
-const dotenv  = require("dotenv").config();
+const dotenv = require("dotenv").config();
 const errorHandler = require("./middleware/errorHandler");
 const cors = require("cors");
-const session = require("express-session")
-const cookiParser = require("cookie-parser")
-const bodyParser = require("body-parser")
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const pgSession = require('connect-pg-simple')(session);
 
 const app = express();
 
 const port = process.env.PORT;
 
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(cors(
-    {origin: ["http://localhost:3000"],
+const db = require('./db'); // Import your PostgreSQL connection
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors({
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true}
-))
-app.use(cookiParser())
+    credentials: true
+}));
+app.use(cookieParser());
 app.use(session({
+    store: new pgSession({
+        pool: db // Pass your PostgreSQL pool here
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
@@ -26,7 +32,7 @@ app.use(session({
         secure: false,
         maxAge: 1000 * 60 * 60 * 24
     }
-}))
+}));
 
 
 app.use("/api/", require("./routes/prescriptionsRouter"));

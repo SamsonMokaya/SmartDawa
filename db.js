fcc1ab1e9901ea9bec52783c58d8b263fa3927de
Samson Mokaya <68181226+SamsonMokaya@users.noreply.github.com>
2023-10-13 09:33:23 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,25 @@
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv').config();
+const { Pool } = require('pg');
+const dotenv = require("dotenv").config();
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
 
+const dbPassword = process.env.DBPASSWORD;
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
+const pool = new Pool({
+  user: 'postgres',
+  password: dbPassword,
   host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'smartdawa',
-  port: 3306,
+  port: 5432,
+  database: 'dbname',
 });
 
-module.exports = pool;
+pool.connect()
+  .then(() => console.log('Connected to PostgreSQL database'))
+  .catch(err => console.error('Error connecting to the database', err));
+
+const sessionStore = new pgSession({
+  pool: pool, // Pass the pool to the session store
+  tableName: 'sessions' // Specify the table name for sessions
+});
+
+module.exports = { pool, sessionStore };
